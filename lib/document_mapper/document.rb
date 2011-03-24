37ab5e7436c6c8fab8f03676292221622905993b
ralph <ralph@rvdh.de>
2011-03-24 20:29:22 +0100

    create attribute methods for each document object

--- lib/document_mapper/document.rb
+++ lib/document_mapper/document.rb
@@ -1,7 +1,7 @@
 module DocumentMapper
-
   class Document
-    attr_accessor :content, :file_path, :data
+
+    attr_accessor :file_path, :attributes, :content
 
     def self.from_file(file_path)
       if !File.exist? file_path
@@ -24,21 +24,33 @@ module DocumentMapper
     def self.select(options = {})
     end
 
+    def define_attribute_method(method_name)
+      self.class.module_eval <<-STR, __FILE__, __LINE__ + 1
+        if method_defined?(:#{method_name})
+          undef :#{method_name}
+        end
+        def #{method_name}
+          self.attributes['#{method_name}']
+        end
+      STR
+    end
+
     def read_yaml
-      @content = File.read(@file_path)
+      @content = File.read(file_path)
 
       if @content =~ /^(---\s*\n.*?\n?)^(---\s*$\n?)/m
         @content = @content[($1.size + $2.size)..-1]
-        @data = YAML.load($1)
+        self.attributes = YAML.load($1)
       end
-      @data ||= {}
-      if !@data.has_key? 'date'
+      self.attributes ||= {}
+      if !self.attributes.has_key? 'date'
         begin
           match = File.basename(@file_path).match(/(\d{4})-(\d{1,2})-(\d{1,2}).*/)
-          @data['date'] = Date.new(match[1].to_i, match[2].to_i, match[3].to_i)
+          self.attributes['date'] = Date.new(match[1].to_i, match[2].to_i, match[3].to_i)
         rescue NoMethodError => err
         end
       end
+      self.attributes.keys.each { |attr| define_attribute_method attr }
     end
   end
 end
--- test/document_mapper_test.rb
+++ test/document_mapper_test.rb
@@ -4,15 +4,24 @@ include DocumentMapper
 describe Document do
   describe 'loading a document from file' do
     before do
-      file_name = 'test/documents/2010-08-08-test-document-file.textile'
-      @document = Document.from_file(file_name)
+      @file_path = 'test/documents/2010-08-08-test-document-file.textile'
+      @document = Document.from_file(@file_path)
     end
 
     it 'should load the document from a yaml file' do
-      assert_equal 1, @document.data['id']
-      assert_equal 'Some fancy title', @document.data['title']
-      assert_equal ['ruby'], @document.data['tags']
-      assert_equal :published, @document.data['status']
+      assert_equal 1, @document.attributes['id']
+      assert_equal 'Some fancy title', @document.attributes['title']
+      assert_equal ['ruby'], @document.attributes['tags']
+      assert_equal :published, @document.attributes['status']
+    end
+
+    it 'should specify attributes from the YAML header' do
+      assert_equal 1, @document.id
+      assert_equal 'Some fancy title', @document.title
+      assert_equal ['ruby'], @document.tags
+      assert_equal :published, @document.status
+      assert_equal '2010-08-08', @document.date.to_s
+      assert_equal @file_path, @document.file_path
     end
   end
 
@@ -23,7 +32,7 @@ describe Document do
     end
 
     # it 'should return the right documents' do
-    #   found_documents = Document.where(:title => @document.data['title']).first
+    #   found_documents = Document.where(:title => @document.attributes['title']).first
     #   assert_equal @document, found_documents
     # end
   end
