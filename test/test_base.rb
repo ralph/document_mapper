4a646e918e16335a084c454860c3b2ef5a11da1d
ralph <ralph@rvdh.de>
2011-03-25 19:18:14 +0100

    fix single test files

--- test/test_base.rb
+++ test/test_base.rb
@@ -1,9 +1,7 @@
 require 'minitest/spec'
 MiniTest::Unit.autorun
-require 'set'
-require 'fileutils'
 
-lib_dir = File.dirname(__FILE__) + '/lib'
+lib_dir = File.expand_path(File.dirname(__FILE__) + '/../lib')
 $LOAD_PATH.unshift lib_dir unless $LOAD_PATH.include?(lib_dir)
 require 'document_mapper'
 TEST_DIR = File.dirname(__FILE__)
